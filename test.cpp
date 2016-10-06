312e75befbca3ef93a1bd170ca8d6512ca495539
sunleaves <hayate.nagi.hayate@gmail.com>
2016-10-06 15:03:22 +0800

    test

---
+++ test.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+	string a;
+	int b;
+	cout<<"enter your neme : ";
+	cin>>a;
+	cout<<"\nAre u smart??(enter 0 or 1):";
+	cin>>b;
+	if (b==0){
+		cout<<a<<" u r stupid!!";
+	}
+else{
+	cout<<a<<" u make me discuse...";
+}	
+}
